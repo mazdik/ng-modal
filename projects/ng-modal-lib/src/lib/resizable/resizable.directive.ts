09fe04b8c102a47c33f0047724f6b6b4b34120ba
Oleg Brezhnev <BrezhnevOS@quarta.su>
2020-12-11 16:41:51 +0300

    package fix

--- projects/ng-modal-lib/src/lib/resizable/resizable.directive.ts
+++ projects/ng-modal-lib/src/lib/resizable/resizable.directive.ts
@@ -15,6 +15,11 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
   @Input() east: boolean;
   @Input() southEast: boolean;
   @Input() ghost: boolean;
+  @Input() southWest: boolean;
+  @Input() west: boolean;
+  @Input() northWest: boolean;
+  @Input() north: boolean;
+  @Input() northEast: boolean;
 
   @Output() resizeBegin: EventEmitter<any> = new EventEmitter();
   @Output() resizing: EventEmitter<ResizableEvent> = new EventEmitter();
@@ -24,9 +29,16 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
   private subscription: Subscription;
   private newWidth: number;
   private newHeight: number;
+  private newLeft: number;
+  private newTop: number;
   private resizingS: boolean; // south
   private resizingE: boolean; // east
   private resizingSE: boolean; // south-east
+  private resizingSW: boolean;
+  private resizingW: boolean;
+  private resizingNW: boolean;
+  private resizingN: boolean;
+  private resizingNE: boolean;
 
   private minWidth: number;
   private maxWidth: number;
@@ -46,6 +58,21 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     }
     if (this.southEast) {
       this.createHandle('resize-handle-se');
+    }
+    if(this.southWest) {
+      this.createHandle('resize-handle-sw');
+    }
+    if(this.west) {
+      this.createHandle('resize-handle-w');
+    }
+    if(this.northWest) {
+      this.createHandle('resize-handle-nw');
+    }
+    if(this.north) {
+      this.createHandle('resize-handle-n');
+    }
+    if(this.northEast) {
+      this.createHandle('resize-handle-ne');
     }
     const computedStyle = window.getComputedStyle(this.element);
     this.minWidth = parseFloat(computedStyle.minWidth);
@@ -68,10 +95,17 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     const isSouth = classList.contains('resize-handle-s');
     const isEast = classList.contains('resize-handle-e');
     const isSouthEast = classList.contains('resize-handle-se');
+    const isSouthWest = classList.contains('resize-handle-sw');
+    const isWest  = classList.contains('resize-handle-w');
+    const isNorthWest = classList.contains('resize-handle-nw');
+    const isNorth = classList.contains('resize-handle-n');
+    const isNorthEast = classList.contains('resize-handle-ne');
 
     const evt = getEvent(event);
     const width = this.element.clientWidth;
     const height = this.element.clientHeight;
+    const left  = this.element.offsetLeft;
+    const top = this.element.offsetTop;
     const screenX = evt.screenX;
     const screenY = evt.screenY;
 
@@ -79,8 +113,8 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     const moveEvent = isTouchEvent ? 'touchmove' : 'mousemove';
     const upEvent = isTouchEvent ? 'touchend' : 'mouseup';
 
-    if (isSouth || isEast || isSouthEast) {
-      this.initResize(event, isSouth, isEast, isSouthEast);
+    if (isSouth || isEast || isSouthEast || isSouthWest || isWest || isNorthWest || isNorth || isNorthEast) {
+      this.initResize(event, isSouth, isEast, isSouthEast, isSouthWest, isWest, isNorthWest, isNorth, isNorthEast);
 
       const mouseup = fromEvent(document, upEvent);
       this.subscription = mouseup
@@ -88,18 +122,22 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
 
       const mouseMoveSub = fromEvent(document, moveEvent)
         .pipe(takeUntil(mouseup))
-        .subscribe((e: MouseEvent | TouchEvent) => this.move(e, width, height, screenX, screenY));
+        .subscribe((e: MouseEvent | TouchEvent) => this.move(e, width, height, top, left, screenX, screenY));
 
       this.subscription.add(mouseMoveSub);
     }
   }
 
-  move(event: MouseEvent | TouchEvent, width: number, height: number, screenX: number, screenY: number): void {
+  move(event: MouseEvent | TouchEvent, width: number, height: number, top: number, left: number, screenX: number, screenY: number): void {
     const evt = getEvent(event);
     const movementX = evt.screenX - screenX;
     const movementY = evt.screenY - screenY;
-    this.newWidth = width + movementX;
-    this.newHeight = height + movementY;
+
+    this.newWidth = width - (this.resizingSW || this.resizingW || this.resizingNW? movementX : -movementX);
+    this.newHeight = height - (this.resizingNW || this.resizingN || this.resizingNE? movementY : -movementY);
+    this.newLeft = left + movementX;
+    this.newTop = top + movementY;
+
     this.resizeWidth(evt);
     this.resizeHeight(evt);
   }
@@ -122,7 +160,15 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     this.element.appendChild(node);
   }
 
-  initResize(event: MouseEvent | TouchEvent, isSouth: boolean, isEast: boolean, isSouthEast: boolean) {
+initResize(event: MouseEvent | TouchEvent, 
+    isSouth: boolean, 
+    isEast: boolean, 
+    isSouthEast: boolean, 
+    isSouthWest: boolean, 
+    isWest: boolean, 
+    isNorthWest: boolean, 
+    isNorth: boolean,
+    isNorthEast: boolean) {
     if (isSouth) {
       this.resizingS = true;
     }
@@ -132,10 +178,30 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     if (isSouthEast) {
       this.resizingSE = true;
     }
+    if (isSouthWest) {
+      this.resizingSW = true;
+    }
+
+    if (isWest) {
+      this.resizingW = true;
+    }
+
+    if (isNorthWest) {
+      this.resizingNW = true;
+    }
+
+    if (isNorth) {
+      this.resizingN = true;
+    }
+
+    if (isNorthEast) {
+      this.resizingNE = true;
+    }
     this.element.classList.add('resizing');
     this.newWidth = this.element.clientWidth;
     this.newHeight = this.element.clientHeight;
-
+    this.newLeft = this.element.offsetLeft;
+    this.newTop  = this.element.offsetTop;
     event.stopPropagation();
     this.resizeBegin.emit();
   }
@@ -144,6 +210,11 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     this.resizingS = false;
     this.resizingE = false;
     this.resizingSE = false;
+    this.resizingSW = false;
+    this.resizingW = false;
+    this.resizingNW = false;
+    this.resizingN = false;
+    this.resizingNE = false;
     this.element.classList.remove('resizing');
     this.resizeEnd.emit({ event: getEvent(event), width: this.newWidth, height: this.newHeight });
   }
@@ -152,28 +223,43 @@ export class ResizableDirective implements OnDestroy, AfterViewInit {
     const overMinWidth = !this.minWidth || this.newWidth >= this.minWidth;
     const underMaxWidth = !this.maxWidth || this.newWidth <= this.maxWidth;
 
-    if (this.resizingSE || this.resizingE) {
+    if (this.resizingSE || this.resizingE || this.resizingNE) {
       if (overMinWidth && underMaxWidth) {
         if (!this.ghost) {
           this.element.style.width = `${this.newWidth}px`;
         }
-        this.resizing.emit({ event, width: this.newWidth, height: this.newHeight, direction: 'horizontal' });
       }
     }
+    if(this.resizingSW || this.resizingW || this.resizingNW) {
+      if (overMinWidth && underMaxWidth) {
+        this.element.style.left = `${this.newLeft}px`;
+        this.element.style.width = `${this.newWidth}px`;
+      }
+    }
+    this.resizing.emit({ event, width: this.newWidth, height: this.newHeight, direction: 'horizontal' });
+
   }
 
   resizeHeight(event: MouseEvent | Touch) {
     const overMinHeight = !this.minHeight || this.newHeight >= this.minHeight;
     const underMaxHeight = !this.maxHeight || this.newHeight <= this.maxHeight;
-
-    if (this.resizingSE || this.resizingS) {
+    if (this.resizingSE || this.resizingS || this.resizingSW) {
       if (overMinHeight && underMaxHeight) {
         if (!this.ghost) {
           this.element.style.height = `${this.newHeight}px`;
         }
-        this.resizing.emit({ event, width: this.newWidth, height: this.newHeight, direction: 'vertical' });
       }
     }
+
+    if (this.resizingNW || this.resizingN || this.resizingNE) {
+      if (overMinHeight && underMaxHeight) {
+        if (!this.ghost) {
+          this.element.style.top = `${this.newTop}px`;
+          this.element.style.height = `${this.newHeight}px`;
+        }
+      }
+    }
+    this.resizing.emit({ event, width: this.newWidth, height: this.newHeight, direction: 'vertical' });
   }
 
 }
--- src/app/panel-demo.component.ts
+++ src/app/panel-demo.component.ts
@@ -3,57 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-panel-demo',
   template: `
-  <button class="dt-button" (click)="modal1.show()">Open panel 1</button>&nbsp;
-  <button class="dt-button" (click)="modal2.show()">Open panel 2</button>&nbsp;
-  <button class="dt-button" (click)="modal3.show()">Open panel 2</button>&nbsp;
-    <app-modal #modal1
-               class="panel-demo1"
-               [maximizable]="true"
-               [backdrop]="false">
-      <ng-container class="app-modal-header">Panel 1</ng-container>
-      <ng-container class="app-modal-body">
-        <h3>MODAL DIALOG</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.</p>
-      </ng-container>
-      <ng-container class="app-modal-footer">
-        <button class="dt-button dt-red">Delete</button>&nbsp;
-        <button class="dt-button dt-green">Save</button>
-        <button class="dt-button dt-blue" style="float: right;" (click)="modal1.hide()">Close</button>
-      </ng-container>
-    </app-modal>
-    <app-modal #modal2
-               class="panel-demo2"
-               [maximizable]="true"
-               [backdrop]="false">
-      <ng-container class="app-modal-header">Panel 2</ng-container>
-      <ng-container class="app-modal-body">
-        <h3>MODAL DIALOG</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.</p>
-      </ng-container>
-      <ng-container class="app-modal-footer">
-        <button class="dt-button dt-red">Delete</button>&nbsp;
-        <button class="dt-button dt-green">Save</button>
-        <button class="dt-button dt-blue" style="float: right;" (click)="modal2.hide()">Close</button>
-      </ng-container>
-    </app-modal>
-    <app-modal #modal3
-               class="panel-demo3"
-               [maximizable]="true"
-               [backdrop]="false">
-      <ng-container class="app-modal-header">Panel 3</ng-container>
-      <ng-container class="app-modal-body">
-        <h3>MODAL DIALOG</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.</p>
-      </ng-container>
-      <ng-container class="app-modal-footer">
-        <button class="dt-button dt-red">Delete</button>&nbsp;
-        <button class="dt-button dt-green">Save</button>
-        <button class="dt-button dt-blue" style="float: right;" (click)="modal3.hide()">Close</button>
-      </ng-container>
-    </app-modal>
+  
   `,
 })
 export class PanelDemoComponent {
